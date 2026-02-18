ac49d6ca15de909763d709c4aa794d27c88ace87
github-newstar <seanrunix@qq.com>
2026-02-19 02:53:01 +0800

    add gtsmamba

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,9 +1,11 @@
 from src.model.baseline_model import BaselineModel
+from src.model.lgmambanet import LGMambaNet
 from src.model.lmambanet import LMambaNet
 from src.model.swin_unetr import SwinUNETRSegModel
 
 __all__ = [
     "BaselineModel",
+    "LGMambaNet",
     "LMambaNet",
     "SwinUNETRSegModel",
 ]
