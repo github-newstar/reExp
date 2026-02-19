8d5710603364618c3cef7122c5b75940daf8a113
shawren <shawren@tencent.com>
2026-02-19 14:17:26 +0800

    add unet3d

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -2,10 +2,12 @@ from src.model.baseline_model import BaselineModel
 from src.model.lgmambanet import LGMambaNet
 from src.model.lmambanet import LMambaNet
 from src.model.swin_unetr import SwinUNETRSegModel
+from src.model.unet3d import UNet3D
 
 __all__ = [
     "BaselineModel",
     "LGMambaNet",
     "LMambaNet",
     "SwinUNETRSegModel",
+    "UNet3D",
 ]
